9fca17c7ad49794c0e033491d23aac57d82d3eb1
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-24 20:59:22 -0600

    styling text banner in app.css along with h1 hr and the p tag within it

--- src/component/landingpage.js
+++ src/component/landingpage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Cell } from 'react-mdl';
+import Image from './me.png';
+
+console.log(Image);
 
 class Landing extends Component {
     render() {
@@ -8,10 +11,24 @@ class Landing extends Component {
             <Grid className="landing-grid">
             <Cell col={12}>
                 <img
-                src=""
-                alt="avatar"
-                className=".avatar-img"
+                src={Image}
+                alt="ME"
+                className="avatar-img"
                 />
+
+                <div className="banner-text">
+                    <h1>Full Stack Web Developer</h1>
+
+                    <hr/>
+
+                    <p>HTML/CSS | Bootstrap | JavaScript | React | Redux |NodeJS | Express | MongoDB</p>
+
+                    <div className="social-links">
+                        <a href="http">
+                            <i className="fa"
+                        </a>
+                    </div>
+                </div>
             </Cell>
             </Grid>
             </div>
