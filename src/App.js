0c9cb07fedab14efd82cdc74da151c8ae70f6c90
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-24 18:37:26 -0600

    created React portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello Emmanuel Stephen</h1>
+    </div>
+  );
+}
+
+export default App;
