279cb1c1b39387647c19a0e640c3aaccac7df0fa
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-24 19:25:17 -0600

    Created main.js in component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from './component/main';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
