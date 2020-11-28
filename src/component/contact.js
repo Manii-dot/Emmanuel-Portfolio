11a4625782503b3b87119180265d9898af2841a9
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-28 13:59:07 -0600

    added contact list and info

--- src/component/contact.js
+++ src/component/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 import Image from './me2.png';
 
 class Contact extends Component {
@@ -13,8 +13,31 @@ class Contact extends Component {
                         alt="professional picture"
                         // style={{height: '250px'}} 
                         />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
+                            Will add the text for later. For now. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+                        </p>
                     </Cell>
-                    <Cell col={6}>OK</Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr/>
+
+                        <div className="contact-list">
+                        <List>
+                            <ListItem>
+                                <ListItemContent className="info">
+                                <i className="fa fa-phone-square" aria-hidden="true"/>    
+                                254-245-5260</ListItemContent>
+                            </ListItem>
+                            
+                            <ListItem>
+                                <ListItemContent className="info">
+                                <i className="fa fa-envelope" aria-hidden="true"/>    
+                                emmanuel.stephen68@gmail.com</ListItemContent>
+                            </ListItem>
+                        </List>
+                        </div>
+                        
+                    </Cell>
                 </Grid>
             </div>
         )
