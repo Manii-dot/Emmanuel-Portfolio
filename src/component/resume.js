e05fa33c9c825f245da2ae07e2d971d0b65698a1
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-29 16:09:51 -0600

    Adding info on resume

--- src/component/resume.js
+++ src/component/resume.js
@@ -21,6 +21,11 @@ class Resume extends Component {
                     <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
                     <p>Will add the text for later. For now. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
                     </p>
+                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                    <h5>Phone</h5>
+                    <p>(254)-245-5260</p>
+                    <h5>Email</h5>
+                    <p>emmanuel.stephen68@gmail.com</p>
                     </Cell>
                     <Cell className="resume-right-col" col={8}>Right Side</Cell>
                 </Grid>           
