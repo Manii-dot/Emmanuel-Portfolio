1f80eb80c1b9d4b64fbbe567317f6b4715daf7a5
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-29 17:14:57 -0600

    Changing My Portfolio link to my Name in the App.js file

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Emmanuel Stephen</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/project">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Emmanuel Stephen</Link>}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/project">Projects</Link>
