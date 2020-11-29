b8f32deb25236c2a93d6c722389d5abcf8ec8754
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-29 16:02:53 -0600

    styling the  css and adding content

--- src/component/resume.js
+++ src/component/resume.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import { Grid, Cell } from 'react-mdl';
+import Image from './me2.png';
 
 class Resume extends Component {
     render() {
         return (
             <div>
                 <Grid>
-                    <Cell col={4}>Left Side</Cell>
+                    <Cell col={4}>
+                    <div style={{textAlign: 'center'}}>
+                    <img 
+                        src={Image}
+                        alt="professional picture" 
+                        style={{height: '200px'}}
+                        />
+                    </div>
+
+                    <h2 style={{paddingTop: '2em'}}>Emmanuel Stephen</h2>
+                    <h4 style={{color: 'grey'}}>Programmer</h4>
+                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                    <p>Will add the text for later. For now. Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+                    </p>
+                    </Cell>
                     <Cell className="resume-right-col" col={8}>Right Side</Cell>
                 </Grid>           
             </div>
